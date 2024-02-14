8cd4cf871d9bbbb4b3e1c8c3f85eebccb8dd9f75
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-02-14 15:48:30 +0000

    Create first functional component by create its file with name FunctionalGreeting and write the functional component in the arrow function method, export it, import it in App.js and render it.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
 
 function App() {
   return (
     <div className="App">
-      <h1>It works!</h1>
+      <FunctionalGreeting />
     </div>
   );
 }
