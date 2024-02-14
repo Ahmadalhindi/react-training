4c9bc8816161ad6d9f7b68d1c2e5bbedf055eefa
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-02-14 19:09:54 +0000

    Class Components: step(1) - Recreate the functionality of the functional greeting with props component, but this time using a class component.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGretting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" />
+      <StatefulGreeting greeting="I'm a stateful class component!" />
     </div>
   );
 }
--- my-app/src/components/FunctionalGreetingWithProps.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-/* function FunctionalGreeting() {
-    return <h1>Hello from React!</h1>
-} */
-const FunctionalGreetingWithProps = (props) => <h1>Hello, {props.greeting}</h1>
-
-export default FunctionalGreetingWithProps
