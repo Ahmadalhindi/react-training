7c35804ab0a49f66f9bf5ef8006e2d18889d9e61
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-02-17 17:28:48 +0000

    class components: step(2) - Adding state to components: ins StatefulGreeting.js insiade class we add constructor method. The constructor include super function and this.state with initial state objects. Then render the return of this.state propeties with heading and button tags.

--- my-app/src/components/StatefulGretting.js
+++ my-app/src/components/StatefulGretting.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state={
+            instroction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
     render() {
-        return <h1>Hello {this.props.greeting}</h1>
+        return(
+            <div>
+                <h1>{this.state.instroction} {this.props.greeting}</h1>
+                <button>{this.state.buttonText}</button>
+            </div>
+        )
     }
 }
 
